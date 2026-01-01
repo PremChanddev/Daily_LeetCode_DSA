770c84422e63adf32085332ea5e1ce31a4e6034b
admin <admin@PREM-WORKSTATIO>
2026-01-01 23:36:40 +0530

    Add project files.

---
+++ Daily_LeetCode_DSA/January/PlusOne.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Daily_LeetCode_DSA.January
+{
+    internal class PlusOne
+    {
+        public static int[] PlusOneMethod(int[] digits)
+        {
+            for (int i = digits.Length - 1; i >= 0; i--)
+            {
+                if (digits[i] < 9)
+                {
+                    digits[i]++;
+                    return digits;
+                }
+                digits[i] = 0;
+            }
+            int[] newNumber = new int[digits.Length + 1];
+            newNumber[0] = 1;
+            return newNumber;
+        }
+    }
+}
---
+++ Daily_LeetCode_DSA/Program.cs
@@ -0,0 +1,13 @@
+﻿using Daily_LeetCode_DSA.January;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        //Day 1: Plus One:66
+        int[] digits = { 9, 9 };
+        Console.WriteLine(string.Join(",", PlusOne.PlusOneMethod(digits)));
+    }
+}
